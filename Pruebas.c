489aa77fac50804edc12cf7c9712004701670159
LILE-Z <LILE-Z>
2023-05-06 21:04:02 -0600

    ascci art at the middle

--- Pruebas.c
+++ Pruebas.c
@@ -1,72 +1,14 @@
 #include <ncurses.h>
-
-void mostrar_opciones(WINDOW* ventana, const char* opciones[], int filas, int columnas, int fila, int columna) {
-    // Borrar la ventana y mostrar las opciones
-    wclear(ventana);
-    // Imprimir el título
-    mvwprintw(ventana, 1, (getmaxx(ventana) - 7) / 2, "Horario");
-
-    // Imprimir las opciones
-    for (int i = 0; i < filas; i++) {
-        for (int j = 0; j < columnas; j++) {
-            mvwprintw(ventana, i + 2, j * 15 + 2, opciones[i * columnas + j]);
-        }
-    }
-
-    // Resaltar la opción seleccionada
-    wattron(ventana, A_REVERSE);
-    mvwprintw(ventana, fila + 2, columna * 15 + 2, opciones[fila * columnas + columna]);
-    wattroff(ventana, A_REVERSE);
-
-    // Actualizar la ventana
-    wrefresh(ventana);
-}
-
-int ejecutar_menu(WINDOW* ventana_opciones, const char* opciones[], int filas, int columnas) {
-    int fila = 0;
-    int columna = 0;
-
-    mostrar_opciones(ventana_opciones, opciones, filas, columnas, fila, columna);
-
-    while (true) {
-        int tecla = getch();
-
-        switch (tecla) {
-            case KEY_UP:
-                fila = (fila - 1 + filas) % filas;
-                break;
-            case KEY_DOWN:
-                fila = (fila + 1) % filas;
-                break;
-            case KEY_LEFT:
-                columna = (columna - 1 + columnas) % columnas;
-                break;
-            case KEY_RIGHT:
-                columna = (columna + 1) % columnas;
-                break;
-            case 10:
-                if (fila * columnas + columna < filas * columnas - 2 && strlen(opciones[fila * columnas + columna]) > 0) {
-                    return fila * columnas + columna;
-                }
-                break;
-            default:
-                break;
-        }
-
-        mostrar_opciones(ventana_opciones, opciones, filas, columnas, fila, columna);
-    }
-}
-
+#include <string.h>
 
 int main() {
-    // LAS OPCIONES QUE ESTEN VACIAS NO SE PODRAN SELECCIONAR
-    const char* opciones[] = {
-        "Opción 1", "Opción 2", "Opción 3",
-        "Opción 4", "Opción 5", "Opción 6",
-        "Opción 7", "Opción 8", "Opción 9",
-        "Opción 10","", ""
-
+    char* title = "Título centrado";
+    char* asciiArt[] = {
+        "   /\\_/\\",
+        "  ( o.o )",
+        "   > ^ <"
     };
+    int numLines = sizeof(asciiArt) / sizeof(asciiArt[0]);
 
     // Inicializar ncurses
     initscr();
@@ -74,19 +16,37 @@ int main() {
     noecho();
     keypad(stdscr, TRUE);
 
-    // Crear la ventana para mostrar las opciones
-    WINDOW* ventana_opciones = newwin(7, 45, (LINES - 7) / 2, (COLS - 45) / 2);
-    
-    refresh(); // Actualizar la pantalla
+    // Obtener tamaño de la ventana
+    int windowHeight, windowWidth;
+    getmaxyx(stdscr, windowHeight, windowWidth);
 
-    // Ejecutar el menú
- int opcionSeleccionada=  ejecutar_menu(ventana_opciones, opciones, 4, 3);
-    printw("La opcion seleccionada es: %d y la opcion %s", opcionSeleccionada, opciones[opcionSeleccionada]);
+    // Calcular posiciones
+    int titleLength = strlen(title);
+    int titleX = (windowWidth - titleLength) / 2;
+    int titleY = windowHeight / 2 - numLines / 2;
+
+    int artX = (windowWidth - strlen(asciiArt[0])) / 2;
+
+    // Crear ventana
+    WINDOW* win = newwin(windowHeight, windowWidth, 0, 0);
+    box(win, 0, 0);
     refresh();
+    // Imprimir título
+    mvwprintw(win, titleY, titleX, "%s", title);
+
+    // Imprimir ASCII art
+    for (int i = 0; i < numLines; i++) {
+        mvwprintw(win, titleY + i + 2, artX, "%s", asciiArt[i]);
+    }
+
+    // Refrescar la ventana
+    wrefresh(win);
+
+    // Esperar la entrada del usuario
     getch();
+
     // Finalizar ncurses
     endwin();
 
     return 0;
 }
-
