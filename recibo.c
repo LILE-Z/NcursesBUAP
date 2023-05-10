92fdfb55d4af6d951776328be53f556f992a0fb7
LILE-Z <LILE-Z>
2023-05-09 21:43:36 -0600

    recibo rdy for implementation

--- recibo.c
+++ recibo.c
@@ -1,28 +1,37 @@
 #include <ncurses.h>
 
-void print_array(WINDOW *window, char *array[], int size) {
-  int i;
-  int sectionHeight = (getmaxy(window) - 2) / size;
-  
-  for (i = 0; i < size; i++) {
-    // Imprimir línea horizontal
-    if (i != 0) {
-      mvwhline(window, i * sectionHeight, 1, ACS_HLINE, getmaxx(window) - 2);
-    }
-    
-    // Imprimir elemento del arreglo
-    mvwprintw(window, i * sectionHeight + 1, 2, "Elemento %d: %s", i + 1, array[i]);
-  }
-}
+void printRecibo(WINDOW *window, char *array[], int size) {
+    int i;
+    int sectionHeight = (getmaxy(window) - 2) / size;
 
+    for (i = 0; i < size; i++) {
+        // Imprimir línea horizontal
+        if (i != 0) {
+            mvwhline(window, i * sectionHeight, 1, ACS_HLINE, getmaxx(window) - 2);
+        }
+
+        // Imprimir elemento del arreglo
+        if (i == 0 || i == size - 1) {
+            wattron(window, A_BOLD| A_REVERSE);
+        }
+
+        mvwprintw(window, i * sectionHeight + 1, 2, "%s", array[i]);
+
+        if (i == 0 || i == size - 1) {
+            wattroff(window, A_BOLD| A_REVERSE);
+        }
+    }
+}
 int main() {
-  WINDOW *window;
+  WINDOW *recibow;
   char *reciboM[] = {
-    "Texto 1",
-    "Texto 2",
-    "Texto 3",
-    "Texto 4",
-    "Texto 5"
+    "Recibo de pago",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "Total a pagar: $ 100.00"
   };
   
   initscr();
@@ -31,13 +40,13 @@ int main() {
   noecho();
   
   // Crear ventana
-  window = newwin(LINES - 4, COLS - 4, 2, 2);
-  box(window, 0, 0);
+  recibow = newwin(LINES - 4, COLS - 4, 2, 2);
+  box(recibow, 0, 0);
   refresh(); 
   // Imprimir el arreglo y las líneas horizontales en la ventana
-  print_array(window, reciboM, sizeof(reciboM) / sizeof(char *));
+  printRecibo(recibow, reciboM, sizeof(reciboM) / sizeof(char *));
   
-  wrefresh(window);
+  wrefresh(recibow);
   
   getch();
   
