e8a4418255842b19dbf6be2b2440f66ccd53f7cb
LILE-Z <LILE-Z>
2023-05-13 13:09:33 -0600

    bug with struct

---
+++ cliente.c
@@ -0,0 +1,41 @@
+#include <ncurses.h>
+#include "recibir_archivo.h"
+#include "esc_lec_salas.h"
+#include <stdio.h>
+#include <string.h>
+void peliculas();
+int main(){
+	recibir_info();
+	lectura();
+	initscr(); // Inicializar ncurses
+    mvprintw(2, 0, "Titulo: %s\n", rSalas[0].pelicula.titulo);
+    refresh(); // Actualizar pantalla
+    getch(); // Esperar una entrada del usuario
+    endwin(); // Finalizar ncurses
+
+	// modificando atributo
+	
+	//char name[20] = "Wong Kar Wai";
+	//strcpy(rSalas[0].pelicula.director, name);
+
+	//printf("%s\n", rSalas[0].pelicula.director);
+
+	
+
+	//escritura();
+
+	//nueva_info();
+
+	return 0;
+}
+// Pruebas con funciones
+void peliculas(){
+printf("Peliculas funcion\n");
+for(int i = 0; i < 5; i++){
+	printf("Pelicula %d\n", i);
+	printf("Titulo: %s\n", rSalas[i].pelicula.titulo);
+	printf("Director: %s\n", rSalas[i].pelicula.director);
+	printf("Horario: %s\n", rSalas[i].pelicula.horario);
+	printf("Precio: %f\n", rSalas[i].pelicula.precio);
+}
+}
